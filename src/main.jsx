d28b836a99dba01b78ba727dedbe8d6a692a75ba
Eason Chang <eason@easonchang.com>
2022-07-26 23:02:14 +0800

    Install @nextui-org/react

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import './index.css'
-
+import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
@@ -7,6 +6,8 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 )
