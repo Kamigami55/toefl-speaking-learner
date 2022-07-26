7c96343912f5df71dda40a4c50595ba4b09bc2ce
Eason Chang <eason@easonchang.com>
2022-07-26 23:22:41 +0800

    Use dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { createTheme, NextUIProvider } from '@nextui-org/react'
 import Button from '@nextui-org/react/button'
+
+const darkTheme = createTheme({
+  type: 'dark',
+})
 
 function App() {
   return (
-    <div>
+    <NextUIProvider theme={darkTheme}>
       <Button>Click me</Button>
-    </div>
+    </NextUIProvider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
@@ -6,8 +5,6 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <App />
   </React.StrictMode>
 )
