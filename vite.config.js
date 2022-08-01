6382ba390a5a6188648e233036f8175b125ccbaa
Eason Chang <eason@easonchang.com>
2022-08-02 00:24:36 +0800

    Fix vite standalone deploy as website bug
    Fix by change vite config
    Make us able to deploy our application as website
    
    https://github.com/alloc/vite-react-jsx/issues/10#issuecomment-1034120883

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+  ],
 })
